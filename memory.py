e3d0c84f43bd2d054f3de284fd176e9eeefd5be2
Tara Simms <31570783+taras9@users.noreply.github.com>
2017-09-03 14:18:49 -0400

    Add files via upload

---
+++ memory.py
@@ -0,0 +1,118 @@
+# implementation of card game - Memory
+
+import simplegui
+import random
+
+card_width = 45
+cards = range(8) + range(8)
+exposed = [False, False, False, False, False, False, False, False, False, 
+           False, False, False, False, False, False, False]
+flipped_card1 = 0
+flipped_card2 = 0
+turns = 0
+
+# helper function to initialize globals
+def new_game():
+    global state, turns
+    state = 0
+    random.shuffle(cards)  
+    turns = 0
+    print cards
+    
+
+     
+# define event handlers
+def mouseclick(pos):
+    # add game state logic here
+    global exposed, revealed_index_numbers, state, turns, flipped_card1, flipped_card2
+    card_index = pos[0]/50
+    print "clicked card index is " + str(card_index)
+    
+    if exposed[card_index] == True:
+        return            
+
+    else:
+        if state == 0:
+            exposed[card_index] = True   
+            flipped_card2 = card_index
+            print "flipped_card2 is " + str(card_index)
+            state = 1
+            print "state = 1a"
+            print " "
+            
+        elif state == 1:        
+            exposed[card_index] = True   
+            flipped_card1 = card_index
+            print "flipped_card1 is " + str(card_index)
+            state = 2
+            print "state = 2"
+            print " "
+        
+        
+        else:
+            #It's here we're in state 2.
+            turns += 1            
+            
+            if cards[flipped_card1] != cards[flipped_card2]:
+                exposed[flipped_card1] = False
+                exposed[flipped_card2] = False
+                state = 1
+                print "state = 1b"
+                print " "                  
+        
+            elif cards[flipped_card1]  == cards[flipped_card2]:
+                exposed[flipped_card1] = True
+                exposed[flipped_card2] = True
+           
+                state = 1
+                print "state = 1c"
+                print " "
+                
+            exposed[card_index] = True   
+            flipped_card2 = card_index
+            print "flipped_card2 is " + str(flipped_card2)     
+                
+                   
+        
+#ignoring previously paired cards        
+#state == 0 is no cards flipped
+#state == 1 is one card flipped
+#state == 2 is two cards flipped
+#there's always one card exposed
+    
+                        
+# cards are logically 50x100 pixels in size    
+def draw(canvas):
+    global exposed, cards
+
+              
+    text_location = [0, 65]  
+    label.set_text("Turns = " + str(turns))
+    
+    for card_index in range(len(cards)):               
+        if exposed[card_index]:                         
+                canvas.draw_text(str(cards[card_index]), (text_location[0] + 15, text_location[1]), 50, 'Tan')
+                text_location[0] += 50         
+        else:                       
+            canvas.draw_polygon([(text_location[0], 0), (text_location[0], 100),
+                                (text_location[0] + 50, 100), (text_location[0] + 50, 0)], 
+                                2, 'White', 'Green')                                                                                       
+            text_location[0] += 50
+       
+
+
+# create frame and add a button and labels
+frame = simplegui.create_frame("Memory", 800, 100)
+frame.add_button("Reset", new_game)
+label = frame.add_label("Turns = 0")
+
+# register event handlers
+frame.set_mouseclick_handler(mouseclick)
+frame.set_draw_handler(draw)
+
+# get things rolling
+new_game()
+frame.start()
+
+
+# Always remember to review the grading rubric
---
+++ rockpaper.py
@@ -0,0 +1,104 @@
+# Rock-paper-scissors-lizard-Spock template
+
+
+# The key idea of this program is to equate the strings
+# "rock", "paper", "scissors", "lizard", "Spock" to numbers
+# as follows:
+#
+# 0 - rock
+# 1 - Spock
+# 2 - paper
+# 3 - lizard
+# 4 - scissors
+
+import random
+
+# helper functions
+
+def name_to_number(name):
+    # delete the following pass statement and fill in your code below
+    if name == "rock":
+        name = 0
+    elif  name == "Spock":
+        name = 1
+    elif  name == "paper":
+        name = 2
+    elif  name == "lizard":
+        name = 3
+    elif  name == "scissors":
+        name =  4     
+    else:
+        name =  "bad name"
+    return name
+
+    # convert name to number using if/elif/else
+    # don't forget to return the result!
+
+
+def number_to_name(number):
+    # delete the following pass statement and fill in your code below
+    if number == 0:
+        name = "rock"
+    elif  number == 1:
+        name = "Spock"
+    elif  number == 2:
+        name = "paper"
+    elif  number == 3:
+        name = "lizard"
+    elif  number == 4:
+        name = "scissors"     
+    else:
+        number = "bad number"
+    return name
+    
+    
+    # convert number to a name using if/elif/else
+    # don't forget to return the result!
+    
+
+def rpsls(player_choice): 
+    # delete the following pass statement and fill in your code below
+ 
+    
+    # print a blank line to separate consecutive games
+    print ""
+
+    # print out the message for the player's choice
+    
+    print "Player chooses " + player_choice
+
+    # convert the player's choice to player_number using the function name_to_number()
+    player_number = name_to_number(player_choice)    
+
+    # compute random guess for comp_number using random.randrange()
+    comp_number = random.randrange(0, 4)
+
+    # convert comp_number to comp_choice using the function number_to_name()
+    comp_choice = number_to_name(comp_number)
+    
+    # print out the message for computer's choice
+    print "Computer chooses " + str(comp_choice)
+
+    # compute difference of comp_number and player_number modulo five
+    difference = (comp_number - player_number) % 5
+
+    
+
+    # use if/elif/else to determine winner, print winner message
+    if comp_number == player_number:
+        print "Player and computer tie!"
+    elif difference == 1 or 2:
+        print "Player wins!"
+    elif difference == 3 or 4:
+        print "Computer wins!"
+    
+# test your code - THESE CALLS MUST BE PRESENT IN YOUR SUBMITTED CODE
+rpsls("rock")
+rpsls("Spock")
+rpsls("paper")
+rpsls("lizard")
+rpsls("scissors")
+
+# always remember to check your completed program against the grading rubric
+
+
