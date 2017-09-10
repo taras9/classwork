4ad1b875b34bfeda47ad4113e644f6bc7e15e9fb
Tara Simms <31570783+taras9@users.noreply.github.com>
2017-09-10 14:11:25 -0400

    Update pong.py

--- pong.py
+++ pong.py
@@ -1,3 +1,4 @@
+
 # Implementation of classic arcade game Pong
 
 import simplegui
@@ -119,17 +120,17 @@ def draw(canvas):
 
     # determine whether paddle and ball collide   
     
-    if (ball_pos[1] >= paddle2_pos - 15):
+    if (ball_pos[1] >= paddle2_pos):
         if (ball_pos[1] <= paddle2_pos + PAD_HEIGHT): 
-            if ball_pos[0] <= PAD_WIDTH
+       #     if ball_pos[0] <= PAD_WIDTH
                 ball_vel[0] = -ball_vel[0]
-    if (ball_pos[1] >= paddle1_pos - 15): 
+    if (ball_pos[1] >= paddle1_pos): 
         if(ball_pos[1] <= paddle1_pos + PAD_HEIGHT):
-            if ball_pos[0] <= PAD_WIDTH
+        #    if ball_pos[0] <= PAD_WIDTH
                 ball_vel[0] = -ball_vel[0]	
     elif ball_pos[1]>PAD_WIDTH:
-        score+=1
-        spawn_ball()
+      #  score+=1
+        spawn_ball(RIGHT)
     
     # draw scores
     
