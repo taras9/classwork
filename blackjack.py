1d47162508c2f4766c234eef0f8c4f8c4148f0a8
Tara Simms <31570783+taras9@users.noreply.github.com>
2017-09-12 11:49:53 -0400

    Update blackjack.py

--- blackjack.py
+++ blackjack.py
@@ -11,6 +11,9 @@ import simplegui
 score = 0
 
 # define globals for cards
+SUITS = ('C', 'S', 'H', 'D')
+RANKS = ('A', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K')
+VALUES = {'A':1, '2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, 'T':10, 'J':10, 'Q':10, 'K':10}
 
 
 # define card class
