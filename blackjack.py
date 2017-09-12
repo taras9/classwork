cae30ff3884c1a017913f5b090359d0e09834957
Tara Simms <31570783+taras9@users.noreply.github.com>
2017-09-12 11:41:41 -0400

    Add files via upload

---
+++ blackjack.py
@@ -0,0 +1,49 @@
+# Blackjack
+
+
+
+# load card sprite - 936x384 - source: jfitz.com
+
+
+# initialize some useful global variables
+
+
+# define globals for cards
+
+
+# define card class
+
+
+# define hand class
+
+        
+# define deck class 
+        
+
+
+#define event handlers for buttons
+
+
+# if busted, assign a message to outcome, update in_play and score
+       
+
+    # if hand is in play, repeatedly hit dealer until his hand has value 17 or more
+
+    # assign a message to outcome, update in_play and score
+
+# draw handler    
+
+    # test to make sure that card.draw works, replace with your code below
+
+
+# initialization frame
+
+#create buttons and canvas callback
+
+
+
+# get things rolling
+
+
+
+# remember to review the gradic rubric
