e6064fa0df9979d2090e68194af6c10032d91ec1
Tara Simms <31570783+taras9@users.noreply.github.com>
2017-09-13 11:59:38 -0400

    Update blackjack.py

--- blackjack.py
+++ blackjack.py
@@ -19,15 +19,48 @@ VALUES = {'A':1, '2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, 'T':10,
 
 
 # define card class
+class Card:
+    def __init__(self, suit, rank):
+        if (suit in SUITS) and (rank in RANKS):
+            self.suit = suit
+            self.rank = rank
+        else:
+            print "Invalid card."
 
+    def __str__(self):
+        return self.suit + self.rank
 
+        
 # define hand class
+class Hand:
+    def __init__(self):
+        self.hand = []	# create Hand object
 
+    def __str__(self):
+        # return a string representation of a hand
+        what_is_in_hand = ""
+        for c in self.hand:
+            what_is_in_hand = what_is_in_hand + " " + str(c)           
+        return "Hand contains " + what_is_in_hand
+
+    def add_card(self, card):
+        self.hand.append(card)	# add a card object to a hand
         
 # define deck class 
+class Deck:
+    def __init__(self):
+        self.deck = [Card(suit, rank) for suit in SUITS for rank in RANKS]
         
-
-
+    def __str__(self):
+        # return a string representing the deck
+        deck_string = ""
+        for card in self.deck:
+            card = str(card)
+            deck_string = deck_string + " " + card
+        return "Deck contains " + deck_string
+    
+print Deck()  
+    
 #define event handlers for buttons
 
 
@@ -39,23 +72,23 @@ VALUES = {'A':1, '2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, 'T':10,
     # assign a message to outcome, update in_play and score
 
 # draw handler    
-def draw(canvas):
+#def draw(canvas):
     # test to make sure that card.draw works, replace with your code below
 
 
 
 # initialization frame
-frame = simplegui.create_frame("Blackjack", 600, 600)
-frame.set_canvas_background("Green")
+#frame = simplegui.create_frame("Blackjack", 600, 600)
+#frame.set_canvas_background("Green")
 
 #create buttons and canvas callback
-frame.add_button("Deal", deal, 200)
-frame.set_draw_handler(draw)
+#frame.add_button("Deal", deal, 200)
+#frame.set_draw_handler(draw)
 
 
 # get things rolling
 
-frame.start()
+#frame.start()
 
 
 # remember to review the gradic rubric
