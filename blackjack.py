1a0c7d92031cce8201fec7511152e927fc420f21
Tara Simms <31570783+taras9@users.noreply.github.com>
2017-09-15 14:44:08 -0400

    Update blackjack.py

--- blackjack.py
+++ blackjack.py
@@ -1,15 +1,20 @@
-# Blackjack
+# Mini-project #6 - Blackjack
 
 import simplegui
-
+import random
 
 # load card sprite - 936x384 - source: jfitz.com
-card_images = simplegui.load_image("http://storage.googleapis.com/codeskulptor-assets/cards_jfitz.png")
-card_back = simplegui.load_image("http://storage.googleapis.com/codeskulptor-assets/card_jfitz_back.png") 
 CARD_SIZE = (72, 96)
+CARD_CENTER = (36, 48)
+card_images = simplegui.load_image("http://storage.googleapis.com/codeskulptor-assets/cards_jfitz.png")
+
+CARD_BACK_SIZE = (72, 96)
+CARD_BACK_CENTER = (36, 48)
+card_back = simplegui.load_image("http://storage.googleapis.com/codeskulptor-assets/card_jfitz_back.png")    
 
 # initialize some useful global variables
-
+in_play = False
+outcome = ""
 score = 0
 
 # define globals for cards
@@ -25,11 +30,23 @@ class Card:
             self.suit = suit
             self.rank = rank
         else:
-            print "Invalid card."
+            self.suit = None
+            self.rank = None
+            print "Invalid card: ", suit, rank
 
     def __str__(self):
         return self.suit + self.rank
 
+    def get_suit(self):
+        return self.suit
+
+    def get_rank(self):
+        return self.rank
+
+    def draw(self, canvas, pos):
+        card_loc = (CARD_CENTER[0] + CARD_SIZE[0] * RANKS.index(self.rank), 
+                    CARD_CENTER[1] + CARD_SIZE[1] * SUITS.index(self.suit))
+        canvas.draw_image(card_images, card_loc, CARD_SIZE, [pos[0] + CARD_CENTER[0], pos[1] + CARD_CENTER[1]], CARD_SIZE)
         
 # define hand class
 class Hand:
@@ -45,11 +62,15 @@ class Hand:
 
     def add_card(self, card):
         self.hand.append(card)	# add a card object to a hand
+
+    
         
 # define deck class 
 class Deck:
     def __init__(self):
         self.deck = [Card(suit, rank) for suit in SUITS for rank in RANKS]
+        
+    
         
     def __str__(self):
         # return a string representing the deck
@@ -59,36 +80,60 @@ class Deck:
             deck_string = deck_string + " " + card
         return "Deck contains " + deck_string
     
-print Deck()  
-    
+
+    def shuffle(self):
+        # shuffle the deck 
+        pass    # use random.shuffle()
+
+    def deal_card(self):
+        pass	# deal a card object from the deck
+
+print Deck() 
+
 #define event handlers for buttons
+def deal():
+    global outcome, in_play
 
+    # your code goes here
+    
+    in_play = True
 
-# if busted, assign a message to outcome, update in_play and score
+def hit():
+    pass	# replace with your code below
+ 
+    # if the hand is in play, hit the player
+   
+    # if busted, assign a message to outcome, update in_play and score
        
-
+def stand():
+    pass	# replace with your code below
+   
     # if hand is in play, repeatedly hit dealer until his hand has value 17 or more
 
     # assign a message to outcome, update in_play and score
 
 # draw handler    
-#def draw(canvas):
+def draw(canvas):
     # test to make sure that card.draw works, replace with your code below
-
+    
+    card = Card("S", "A")
+    card.draw(canvas, [300, 300])
 
 
 # initialization frame
-#frame = simplegui.create_frame("Blackjack", 600, 600)
-#frame.set_canvas_background("Green")
+frame = simplegui.create_frame("Blackjack", 600, 600)
+frame.set_canvas_background("Green")
 
 #create buttons and canvas callback
-#frame.add_button("Deal", deal, 200)
-#frame.set_draw_handler(draw)
+frame.add_button("Deal", deal, 200)
+frame.add_button("Hit",  hit, 200)
+frame.add_button("Stand", stand, 200)
+frame.set_draw_handler(draw)
 
 
 # get things rolling
-
-#frame.start()
+deal()
+frame.start()
 
 
 # remember to review the gradic rubric
